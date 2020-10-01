d6859e68b0a8f0cbc2d459c21240b2e3efb9edf2
joseph-fusco <joseph.fusco@workday.com>
2020-10-01 14:06:54 -0700

    fixing the things

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ station = Train.Station()
 
 @app.route("/", methods=['POST'])
 def TrainStation():
+    print("hit the train station")
     #TODO: Set auth to check request is coming from slack
     token = request.form.get('token')
     message = request.form.get('text')
@@ -16,13 +17,17 @@ def TrainStation():
         "text": Train.Handler(station, user_name, message),
         "response_type": 'in_channel'
     }
-    return Response(response=json.dumps(data), status=200, mimetype="application/json")
+    return Response(response=json.dumps(data), status=200, mimetype="application/json#")
 
 
-@app.route("/test", methods=['GET'])
+#@app.route("/test", methods=['GET'])
+@app.route('/')
 def HelloWorld():
+    print("hellow world")
     return "Everything OK!"
 
 
 if __name__ == '__main__':
-    app.run()
+    print("hello!")
+    app.run(debug=True)
+
