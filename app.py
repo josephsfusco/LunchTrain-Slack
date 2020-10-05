771a477934daff14f53a3ffc77a4ac7472f4a042
joseph-fusco <joseph.fusco@workday.com>
2020-10-05 14:13:50 -0700

    fixing response hopefully...again

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ import json
 app = Flask(__name__)
 station = Train.Station()
 
-
 @app.route("/", methods=['POST'])
 def TrainStation():
-    print("hit the train station")
     #TODO: Set auth to check request is coming from slack
     token = request.form.get('token')
     message = request.form.get('text')
@@ -17,7 +15,7 @@ def TrainStation():
         "text": Train.Handler(station, user_name, message),
         "response_type": 'in_channel'
     }
-    return Response(response=json.dumps(data.text), status=200, mimetype="application/json#")
+    return Response(response=json.dumps(data), status=200, mimetype="application/json")
 
 
 #@app.route("/test", methods=['GET'])
