96f47784b4543a55ab4f75b22d589d459124d203
joseph-fusco <joseph.fusco@workday.com>
2020-10-05 15:00:11 -0700

    response update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def TrainStation():
     message = request.form.get('text')
     user_name = request.form.get('user_name')
     data = {
-        "text": " rusty is koty 2020 :crown:",
+        "text": "@rusty is koty 2020 :crown::verified:",
 #        "text": Train.Handler(station, user_name, message),
         "response_type": 'in_channel'
     }
