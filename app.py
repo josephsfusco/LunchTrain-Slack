b72cd470e98a091c9c0cbf38e7dba43878631769
joseph-fusco <joseph.fusco@workday.com>
2020-10-05 14:08:04 -0700

    fixing response hopefully...again

--- app.py
+++ app.py
@@ -7,32 +7,17 @@ station = Train.Station()
 
 
 @app.route("/", methods=['POST'])
-def postToSlack():
-
-    webhook_url = "https://hooks.slack.com/services/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"
-    
-    slack_data = {
-        'text': "hello world :crown::crown:", 
-        }
-
-    response = requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
-
+def TrainStation():
+    print("hit the train station")
+    #TODO: Set auth to check request is coming from slack
+    token = request.form.get('token')
+    message = request.form.get('text')
+    user_name = request.form.get('user_name')
+    data = {
+        "text": Train.Handler(station, user_name, message),
+        "response_type": 'in_channel'
+    }
     return Response(response=json.dumps(data.text), status=200, mimetype="application/json#")
-
-
-
-
-#def TrainStation():
-#    print("hit the train station")
-#    #TODO: Set auth to check request is coming from slack
-#    token = request.form.get('token')
-#    message = request.form.get('text')
-#    user_name = request.form.get('user_name')
-#    data = {
-#        "text": Train.Handler(station, user_name, message),
-#        "response_type": 'in_channel'
-#    }
-#    return Response(response=json.dumps(data.text), status=200, mimetype="application/json#")
 
 
 #@app.route("/test", methods=['GET'])
