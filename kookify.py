8d79cfa016836d5c28cea0bbb03b25281425f2e4
joseph-fusco <joseph.fusco@workday.com>
2020-10-05 20:46:11 -0700

    adding kookify.py

---
+++ kookify.py
@@ -0,0 +1,19 @@
+import threading
+import time
+import requests
+import json
+
+
+def Handler(token, message, user_name):
+    if (message == "who is koty?" or message == "who is koty"): 
+        response = {
+            "text" : "@rusty is KOTY",
+            "user" : "U010YCYF3SM",
+            "response_type" : "in_channel"
+        }
+    else: 
+        response = {
+            "text" : "hello world!",
+            "response_type" : "in_channel"
+        }
+    return response 
